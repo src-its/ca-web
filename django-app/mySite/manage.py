fa747d97ffae0dd949f6bfef3bc17a510916c1fa
root <root@localhost.localdomain>
2016-03-03 10:56:32 -0500

    Cleared some dead directories. Still trying to fix some compatibility issues with an older version of django and the current version.

--- django-app/mySite/manage.py
+++ django-app/mySite/manage.py
@@ -1,9 +1,9 @@
-#!~/mysite/venv/bin/python
+#!/usr/bin/env python
 import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mySite.settings")
 
     from django.core.management import execute_from_command_line
 
--- django-app/mySite/mySite/urls.py
+++ django-app/mySite/mySite/urls.py
@@ -1,4 +1,4 @@
-"""mysite URL Configuration
+"""mySite URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.9/topics/http/urls/
@@ -13,10 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include
+from django.conf.urls import url, include, patterns
 from django.contrib import admin
 
-urlpatterns = [
-    url(r'^polls/', include('polls.urls')),
-    url(r'^admin/', admin.site.urls)
+urlpatterns = patterns[
+	url(r'^admin/', include(admin.site.urls)),
+	url(r'^rango/', include('rango.urls')),
 ]
