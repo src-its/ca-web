c9f0d205dc2436ecaaf2a1d8ceb064b43ca51808
Connor Lehman <clehman13@simons-rock.edu>
2016-03-03 11:11:17 -0500

    Turns out that I cannot spell. Managed to fix a spelling mistake (not sure where, though). Found help at https://docs.djangoproject.com/en/1.9/intro/tutorial01/

--- django-app/mySite/rango/urls.py
+++ django-app/mySite/rango/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, url
 from rango import views
 
-urlpatterns = patterns('',
-	url(r'^$', views.index, name='index'))
+urlpatterns = [
+	url(r'^$', views.index, name='index')]
